1fe70df325e43915d59029d8c2bdd943880041cb
Zdenek Styblik <stybla@turnovfree.net>
2012-03-20 05:46:09 +0100

    Change /bin/bash -> /bin/sh kvm-wrapper start-up Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Commit changes /bin/bash to /bin/sh and all that's necessary to do so.
    * replaced % echo ; -> % printf ;
    * clean-ups
    * got rid of % pcgrep; and replaced with % grep;
    * hopefully more portability:

--- startup/kvm-wrapper-startup.sh
+++ startup/kvm-wrapper-startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          kvm-wrapper
 # Required-Start:    $remote_fs $syslog
@@ -13,93 +13,117 @@
 # -- bencoh, 2009/08/11
 # -- Asmadeus, 2011/06
 
-SCRIPTNAME=/etc/init.d/kvm-wrapper
+SCRIPTNAME="/etc/init.d/kvm-wrapper"
 
-. /lib/lsb/init-functions
+. "/lib/lsb/init-functions"
 
-KVM_WRAPPER_DIR=/usr/share/kvm-wrapper
-KVM_WRAPPER="$KVM_WRAPPER_DIR"/kvm-wrapper.sh
-KVM_VM_LIST="$KVM_WRAPPER_DIR"/startup/startup-list
+KVM_WRAPPER_DIR="/usr/share/kvm-wrapper"
+KVM_WRAPPER="${KVM_WRAPPER_DIR}/kvm-wrapper.sh"
+KVM_VM_LIST="${KVM_WRAPPER_DIR}/startup/startup-list"
 
 start_vm()
 {
-	VM_NAME="$1"
-	log_begin_msg "Starting up VM : $VM_NAME ..."
-	$KVM_WRAPPER screen "$VM_NAME"
-	EXITNUM="$?"
-	echo $EXITNUM
+	VM_NAME=${1:-''}
+	if [ -z "${VM_NAME}" ]; then
+		printf "VM name expected, but none given.\n" 1>&2
+		return 1
+	fi
+	log_begin_msg "Starting up VM: $VM_NAME ..."
+	EXITNUM=0
+	$KVM_WRAPPER screen "$VM_NAME" || EXITNUM=$?
+	printf "%s\n" "$EXITNUM"
 	case "$EXITNUM" in 
 		0) log_end_msg 0 ;;
 		*) log_end_msg 1 ;;
 	esac
 	return 0
-}
+} # start_vm()
 
-stop_vm ()
+stop_vm()
 {
-	VM_NAME="$1"
-	log_begin_msg "Stopping VM : $VM_NAME ..."
+	VM_NAME=${1:-''}
+	if [ -z "${VM_NAME}" ]; then
+		printf "VM name expected, but none given.\n" 1>&2
+		return 1
+	fi
+	log_begin_msg "Stopping VM: $VM_NAME ..."
 	"$KVM_WRAPPER" stop "$VM_NAME"
 	log_end_msg 0
-}
+} # stop_vm()
 
 do_start()
 {
-if [[ ! -f /usr/share/kvm-wrapper/kvm-wrapper.sh ]]; then
-	log_begin_msg "Mounting /usr/share/kvm-wrapper since it doesn't seem here"
-	echo
-	mount /usr/share/kvm-wrapper
-	sleep 3
-fi
+	if [ ! -f /usr/share/kvm-wrapper/kvm-wrapper.sh ]; then
+		log_begin_msg "Mounting /usr/share/kvm-wrapper since it doesn't seem here"
+		printf "\n"
+		mount /usr/share/kvm-wrapper
+		sleep 3
+	fi
 
-echo cleaning old pid files for `hostname -s`
-rm -vf /usr/share/kvm-wrapper/run/`hostname -s`*
+	printf "cleaning old pid files for %s\n" $(hostname -s)
+	rm -vf /usr/share/kvm-wrapper/run/$(hostname -s)*
 
-grep -E -v '^#' "$KVM_VM_LIST" |
-while read line
-do
-	pcregrep "^KVM_CLUSTER_NODE=\"?`hostname -s`" $KVM_WRAPPER_DIR/vm/$line-vm >&/dev/null && \
-	start_vm "$line"
-done
-}
+	for line in $(grep -E -v '^#' "$KVM_VM_LIST"); do
+		printf "%s" "${line}" | grep -E -e "^KVM_CLUSTER_NODE=\"?$(hostname -s)" \
+			"${KVM_WRAPPER_DIR}/vm/${line}-vm" &> /dev/null && \
+			start_vm "$line"
+	done
+} # do_start()
 
 do_stop()
 {
-"$KVM_WRAPPER" list|pcregrep "Running\ton (`hostname -s`|local)"|awk '{print $1}'|
-while read line
-do
-	stop_vm "$line"
-done
-}
+	for vmname in $($KVM_WRAPPER list |\
+		grep -E -e "Running[[:space:]]on ($(hostname -s)|local)" |\
+		awk '{ print $1 }'); do
+		stop_vm "$vmname"
+	done
+} # do_stop
 
-case "$1" in
-  start)
-	log_begin_msg "Autostarting VMs (kvm-wrapper) ..."
-	echo
-	do_start
-	case "$?" in
-		0|1) log_end_msg 0 ;;
-		2) log_end_msg 1 ;;
-	esac
+if [ ! -d "${KVM_WRAPPER_DIR}" ]; then
+	printf "Directory '%s' doesn't seem to exist.\n" "${KVM_WRAPPER_DIR}" 1>&2
+	return 1
+fi
+if [ ! -x "${KVM_WRAPPER}" ]; then
+	printf "Script '%s' either doesn't exist or is not executable.\n" \
+		"${KVM_WRAPPER}" 1>&2
+	return 1
+fi
+
+ACTION=${1:-''}
+case "$ACTION" in
+	start)
+		log_begin_msg "Autostarting VMs (kvm-wrapper) ..."
+		printf "\n"
+		do_start
+		case "$?" in
+			0|1) log_end_msg 0 ;;
+			2) log_end_msg 1 ;;
+		esac
+		;;
+	stop)
+		log_begin_msg "Shutting down autostarted VMs (kvm-wrapper) ..."
+		printf "\n"
+		do_stop
+		case "$?" in
+			0|1) log_end_msg 0 ;;
+			2) log_end_msg 1 ;;
+		esac
+		;;
+	restart|force-reload)
+		;;
+	start-vm)
+		VM_NAME=${2:-''}
+		if [ -z "${VM_NAME}" ]; then
+			printf "VM name expected, but none given.\n" 1>&2
+			return 1
+		fi
+		start_vm "$VM_NAME"
 	;;
-  stop)
-	log_begin_msg "Shutting down autostarted VMs (kvm-wrapper) ..."
-	echo
-	do_stop
-	case "$?" in
-		0|1) log_end_msg 0 ;;
-		2) log_end_msg 1 ;;
-	esac
-	;;
-  restart|force-reload)
-	;;
-  start-vm)
-  	start_vm "$2"
-	;;
-  *)
-	echo "Usage: $SCRIPTNAME {start|stop}" >&2
-	echo "       $SCRIPTNAME start-vm xxxxx" >&2
-	exit 3
+	*)
+		printf "Usage: %s start\n" "${SCRIPTNAME}" 1>&2
+		printf "       %s stop\n" "${SCRIPTNAME}" 1>&2
+		printf "       %s start-vm VM_NAME\n" "${SCRIPTNAME}" 1>&2
+		exit 3
 	;;
 esac
 
