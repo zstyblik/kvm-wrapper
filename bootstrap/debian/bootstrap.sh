8781a5b910d3defff97ef56f3ad4e2cc7ab9a4c3
Zdenek Styblik <stybla@turnovfree.net>
2012-03-20 06:07:41 +0100

    Change /bin/bash -> /bin/sh in bootstrap.sh Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Again, the aim is more script portability.
    * replaced % echo ; -> % printf;
    * replaced Bashisms
    * code formatting - indentations, long lines

--- bootstrap/debian/bootstrap.sh
+++ bootstrap/debian/bootstrap.sh
@@ -1,24 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Bootstrap VM
 # Copyright (C) 2011 Benjamin Cohen <bencoh@codewreck.org>
 #                    Dominique Martinet <asmadeus@codewreck.org>
 # Published under the WTFPLv2 (see LICENSE)
 
-if [[ "`uname -m`" == "x86_64" ]]; then
+ARCH=${ARCH:-$(uname -m)}
+ARCH_SUFFIX="686"
+DPKG_ARCH="i386"
+
+if [ "$ARCH" = "x86_64" ]; then
 	ARCH_SUFFIX="amd64"
 	DPKG_ARCH="amd64"
-else
-	ARCH_SUFFIX="686"
-	DPKG_ARCH="i386"
 fi
 
 ### Configuration
 BOOTSTRAP_LINUX_IMAGE="linux-image-$ARCH_SUFFIX"
 BOOTSTRAP_DEBIAN_MIRROR=${BOOTSTRAP_DEBIAN_MIRROR:-"http://ftp.fr.debian.org/debian/"}
 #BOOTSTRAP_FLAVOR=${BOOTSTRAP_FLAVOR:-lenny}
-BOOTSTRAP_EXTRA_PKGSS="vim-nox,htop,screen,less,bzip2,bash-completion,locate,acpid,acpi-support-base,bind9-host,openssh-server,locales,ntp,busybox,$BOOTSTRAP_LINUX_IMAGE"
-if [[ "$BOOTSTRAP_PARTITION_TYPE" == "msdos" ]]; then
+BOOTSTRAP_EXTRA_PKGSS="vim-nox,htop,screen,less,bzip2,bash-completion,locate,\
+acpid,acpi-support-base,bind9-host,openssh-server,locales,ntp,busybox,\
+$BOOTSTRAP_LINUX_IMAGE"
+if [ "$BOOTSTRAP_PARTITION_TYPE" = "msdos" ]; then
 	BOOTSTRAP_EXTRA_PKGSS+=",grub"
 fi
 BOOTSTRAP_CONF_DIR="$BOOTSTRAP_DIR/$BOOTSTRAP_DISTRIB/conf"
@@ -27,22 +30,25 @@ BOOTSTRAP_INITRD="$BOOT_IMAGES_DIR/initrd.img-$ARCH_SUFFIX"
 BOOTSTRAP_CACHE="$CACHE_DIR/$BOOTSTRAP_FLAVOR-$DPKG_ARCH-debootstrap.tar"
 ### 
 
-function bs_copy_from_host()
+bs_copy_from_host()
 {
-	local FILE="$1"
+	local FILE=${1:-''}
+	if [ -z "${FILE}" ]; then
+		return 1
+	fi
 	cp -rf "$FILE" "$MNTDIR/$FILE"
-}
+} # bs_copy_from_host()
 
-function bs_copy_conf_dir()
+bs_copy_conf_dir()
 {
 	cp -rf "$BOOTSTRAP_CONF_DIR/"* "$MNTDIR/"
-}
+} # bs_copy_conf_dir()
 
-function bootstrap_fs()
+bootstrap_fs()
 {
 
 	check_create_dir "$LOGDIR"
-	local LOGFILE="$LOGDIR/$VM_NAME-boostrap-`date +%Y-%m-%d-%H:%M:%S`"
+	local LOGFILE="$LOGDIR/$VM_NAME-boostrap-$(date +%Y-%m-%d-%H:%M:%S)"
 
 #npipe="/tmp/$$-pipe.tmp"
 #CLEANUP+=("rm -f $npipe")
@@ -51,22 +57,25 @@ function bootstrap_fs()
 #exec 1>&-
 #exec 1>$npipe
 
-# well - this or { ... } |tee -a "$LOGFILE" - but you loose environment there, so this sucks. gotta cut it in about five pieces and it's ugly :P
+# well - this or { ... } |tee -a "$LOGFILE" - but you loose environment there,
+# so this sucks. gotta cut it in about five pieces and it's ugly :P
 
-	test_file "$BOOTSTRAP_KERNEL" || fail_exit "Couldn't find bootstrap kernel : $BOOTSTRAP_KERNEL"
-	test_file "$BOOTSTRAP_INITRD" || fail_exit "Couldn't find bootstrap initrd : $BOOTSTRAP_INITRD"
+	test_file "$BOOTSTRAP_KERNEL" ||\
+		fail_exit "Couldn't find bootstrap kernel : $BOOTSTRAP_KERNEL"
+	test_file "$BOOTSTRAP_INITRD" ||\
+		fail_exit "Couldn't find bootstrap initrd : $BOOTSTRAP_INITRD"
 
-	MNTDIR="`mktemp -d`"
+	MNTDIR=$(mktemp -d)
 	CLEANUP+=("rmdir $MNTDIR")
-	local DISKDEV=$1
-	local PARTDEV=$1
+	local DISKDEV=${1:-''}
+	local PARTDEV=${1:-''}
 
 	local rootdev="LABEL=rootdev"
 	local swapdev
 	local swapuuid
 
-	if [[ "$BOOTSTRAP_PARTITION_TYPE" == "msdos" ]]; then
-		if [[ -n "$SWAP_SIZE" ]]; then
+	if [ "$BOOTSTRAP_PARTITION_TYPE" = "msdos" ]; then
+		if [ -n "$SWAP_SIZE" ]; then
 			sfdisk -D -H 255 -S 63 -uM --Linux "$DISKDEV" <<EOF
 ,$ROOT_SIZE,L,*
 ,,S
@@ -75,16 +84,17 @@ EOF
 			sfdisk -D -H 255 -S 63 -uM --Linux "$DISKDEV" <<EOF
 ,,L,*
 EOF
-		fi
-		PARTDEV=`map_disk $DISKDEV`
+		fi # if [ -n "$SWAP_SIZE" ]; then
+		PARTDEV=$(map_disk $DISKDEV)
 
-		if [[ -n "$SWAP_SIZE" ]]; then
-			swapdev="${PARTDEV:0:$((${#PARTDEV}-1))}2"
-			swap_uuid=`mkswap -f $swapdev|grep -o -e 'UUID=.*'`
-		fi
+		if [ -n "$SWAP_SIZE" ]; then
+			swapdev=$(printf "%s" "${PARTDEV}" | | \
+				awk '{ swapdev=substr($0, 0, length($0)); printf("%s2", swapdev); }')
+			swap_uuid=$(mkswap -f "$swapdev" | grep -o -e 'UUID=.*')
+		fi # if [ -n "$SWAP_SIZE" ]; then
 
 		CLEANUP+=("unmap_disk $DISKDEV")
-	fi
+	fi # if [ "$BOOTSTRAP_PARTITION_TYPE" = "msdos" ]; then
 
 	mkfs.ext3 -L rootdev "$PARTDEV"
 	
@@ -93,31 +103,37 @@ EOF
 	CLEANUP+=("umount $MNTDIR")	
 	CLEANUP+=("sync")
 
-	echo
-	echo
+	printf "\n\n"
 
 	# Debootstrap cache
 	local DEBOOTSTRAP_CACHE_OPTION=""
-	if [[ -n "$BOOTSTRAP_CACHE" ]]; then
-		test_file_rw "$BOOTSTRAP_CACHE" && find "$BOOTSTRAP_CACHE" -mtime +15 -exec rm {} \;
-		if ! test_file_rw "$BOOTSTRAP_CACHE"; then
-			echo "Debootstrap cache either absent or to old : building a new one ..."
-			eval debootstrap --arch $DPKG_ARCH --make-tarball "$BOOTSTRAP_CACHE" --include="$BOOTSTRAP_EXTRA_PKGSS" "$BOOTSTRAP_FLAVOR" "$MNTDIR" "$BOOTSTRAP_DEBIAN_MIRROR" || true
-		fi
+	if [ -n "$BOOTSTRAP_CACHE" ]; then
+		test_file_rw "$BOOTSTRAP_CACHE" &&\
+			find "$BOOTSTRAP_CACHE" -mtime +15 -exec rm {} \;
+
+		if ! test_file_rw "$BOOTSTRAP_CACHE" ; then
+			printf \
+				"Debootstrap cache either absent or to old : building a new one ...\n"
+			eval debootstrap --arch $DPKG_ARCH --make-tarball "$BOOTSTRAP_CACHE" \
+				--include="$BOOTSTRAP_EXTRA_PKGSS" "$BOOTSTRAP_FLAVOR" "$MNTDIR" \
+				"$BOOTSTRAP_DEBIAN_MIRROR" || true
+		fi # if ! test_file_rw "$BOOTSTRAP_CACHE" ; then
 		if test_file "$BOOTSTRAP_CACHE"; then
-			echo "Using debootstrap cache : $BOOTSTRAP_CACHE"
+			printf "Using debootstrap cache: %s\n" "$BOOTSTRAP_CACHE"
 			DEBOOTSTRAP_CACHE_OPTION="--unpack-tarball \"$BOOTSTRAP_CACHE\""
 		else
-			echo "Building debootstrap cache failed."
-		fi
-	fi
+			printf "Building debootstrap cache failed.\n"
+		fi # if test_file "$BOOTSTRAP_CACHE"; then
+	fi # if [ -n "$BOOTSTRAP_CACHE" ]; then
 
 	# Now build our destination
-	eval debootstrap --arch $DPKG_ARCH "$DEBOOTSTRAP_CACHE_OPTION" --foreign --include="$BOOTSTRAP_EXTRA_PKGSS" "$BOOTSTRAP_FLAVOR" "$MNTDIR" "$BOOTSTRAP_DEBIAN_MIRROR"
+	eval debootstrap --arch $DPKG_ARCH "$DEBOOTSTRAP_CACHE_OPTION" \
+		--foreign --include="$BOOTSTRAP_EXTRA_PKGSS" "$BOOTSTRAP_FLAVOR" \
+		"$MNTDIR" "$BOOTSTRAP_DEBIAN_MIRROR"
 
 	# Fix for linux-image module which isn't handled correctly by debootstrap
-	echo "warn_initrd = no" > "$MNTDIR/etc/kernel-img.conf"
-	echo "do_symlinks = no" >> "$MNTDIR/etc/kernel-img.conf"
+	printf "warn_initrd = no\n" > "$MNTDIR/etc/kernel-img.conf"
+	printf "do_symlinks = no\n" >> "$MNTDIR/etc/kernel-img.conf"
 
 
 	# init script to be run on first VM boot
@@ -131,12 +147,12 @@ cat /proc/mounts
 /debootstrap/debootstrap --second-stage
 mount -nt proc proc /proc
 
-echo -e '\n\n\n'
+printf '\n\n\n'
 
 {
 EOF
 
-	if [[ "$BOOTSTRAP_PARTITION_TYPE" == "msdos" ]]; then
+	if [ "$BOOTSTRAP_PARTITION_TYPE" = "msdos" ]; then
 		cat >> "$BS_FILE" << EOF
 /usr/sbin/grub-install /dev/[vh]da
 sed -i -e 's/#\(GRUB_TERMINAL=console\)/\1/' /etc/default/grub
@@ -144,8 +160,8 @@ sed -i -e 's/#\(GRUB_TERMINAL=console\)/\1/' /etc/default/grub
 EOF
 	fi
 
-	if [[ -n "$BOOTSTRAP_FIRSTRUN_COMMAND" ]]; then
-		echo eval "$BOOTSTRAP_FIRSTRUN_COMMAND" >> "$BS_FILE"
+	if [ -n "$BOOTSTRAP_FIRSTRUN_COMMAND" ]; then
+		printf "eval %s\n" "$BOOTSTRAP_FIRSTRUN_COMMAND" >> "$BS_FILE"
 	fi
 
 	cat >> "$BS_FILE" << EOF
@@ -159,7 +175,7 @@ EOF
 
 	chmod +x "$BS_FILE"
 
-	if [[ -n "$BOOTSTRAP_PRERUN_COMMAND" ]]; then
+	if [ -n "$BOOTSTRAP_PRERUN_COMMAND" ]; then
 		eval "$BOOTSTRAP_PRERUN_COMMAND"
 	fi
 
@@ -169,8 +185,8 @@ EOF
 
 	# Start VM to debootstrap, second stage
 	desc_update_setting "KVM_NETWORK_MODEL" "virtio" #put vhost_net if supported
-	test_blockdev "$KVM_DISK1" \
-		&& desc_update_setting "KVM_DRIVE_IF" "virtio-blk-pci,scsi=off"
+	test_blockdev "$KVM_DISK1" &&\
+		desc_update_setting "KVM_DRIVE_IF" "virtio-blk-pci,scsi=off"
 	desc_update_setting "KVM_KERNEL" "$BOOTSTRAP_KERNEL"
 	desc_update_setting "KVM_INITRD" "$BOOTSTRAP_INITRD"
 	desc_update_setting "KVM_APPEND" "root=$rootdev ro init=/bootstrap-init.sh"
@@ -187,73 +203,75 @@ EOF
 
 	cat "$MNTDIR/var/log/bootstrap.log" >> "$LOGFILE"
 
-{
-	rm "$BS_FILE"
+	{
+		rm "$BS_FILE"
 	
-	# Copy some files/configuration from host
-	bs_copy_from_host /etc/hosts
-	bs_copy_from_host /etc/resolv.conf
-	bs_copy_from_host /etc/timezone || true
-	bs_copy_from_host /etc/localtime
+		# Copy some files/configuration from host
+		bs_copy_from_host /etc/hosts
+		bs_copy_from_host /etc/resolv.conf
+		bs_copy_from_host /etc/timezone || true
+		bs_copy_from_host /etc/localtime
 
 
-	echo "$VM_NAME" > "$MNTDIR/etc/hostname"
-	# Custom files
-	bs_copy_conf_dir
+		printf "%s\n" "$VM_NAME" > "$MNTDIR/etc/hostname"
+		# Custom files
+		bs_copy_conf_dir
 	
-	# fstab
-	cat > "$MNTDIR/etc/fstab" << EOF
+		# fstab
+		cat > "$MNTDIR/etc/fstab" << EOF
 # <file system>	<mount point>	<type>	<options>	<dump>	<pass>
 $rootdev	/		ext3	errors=remount-ro	0	1
 proc		/proc	proc	defaults			0	0
 sysfs		/sys	sysfs	defaults			0	0
 EOF
 	
-	if [[ -n "$swap_uuid" ]]; then
-		echo "$swap_uuid		none	swap	sw	0	0" >> "$MNTDIR/etc/fstab"
-	fi
+		if [ -n "$swap_uuid" ]; then
+			printf "%s		none	swap	sw	0	0\n" "$swap_uuid" >> "$MNTDIR/etc/fstab"
+		fi
 
 
-	# interfaces
-	local IF_FILE="$MNTDIR/etc/network/interfaces"
-	cat > "$IF_FILE" << EOF
+		# interfaces
+		local IF_FILE="$MNTDIR/etc/network/interfaces"
+		cat > "$IF_FILE" << EOF
 auto lo
 iface lo inet loopback
 
 auto eth0
 EOF
-	if [[ -n "$BOOTSTRAP_NET_ADDR" ]]; then
-		cat >> "$IF_FILE" << EOF	
+		if [ -n "$BOOTSTRAP_NET_ADDR" ]; then
+			cat >> "$IF_FILE" << EOF	
 iface eth0 inet static
 	address $BOOTSTRAP_NET_ADDR
 	netmask $BOOTSTRAP_NET_MASK
 	network $BOOTSTRAP_NET_NW
 	gateway $BOOTSTRAP_NET_GW
 EOF
-	else
-		cat >> "$IF_FILE" << EOF
+		else
+			cat >> "$IF_FILE" << EOF
 iface eth0 inet dhcp
 EOF
-	fi
+		fi # if [ -n "$BOOTSTRAP_NET_ADDR" ]; then
 
-	sed -i -e 's/root:\*:/root::/' "$MNTDIR/etc/shadow" # squeeze sucks. no, really, I mean it.
+		# squeeze sucks. no, really, I mean it.
+		sed -i -e 's/root:\*:/root::/' "$MNTDIR/etc/shadow"
 
-	sed -i -e "s@DEBIAN_MIRROR@$BOOTSTRAP_DEBIAN_MIRROR@" "$MNTDIR/etc/apt/sources.list"
-	sed -i -e "s/FLAVOR/$BOOTSTRAP_FLAVOR/" "$MNTDIR/etc/apt/sources.list"
+		sed -i -e "s@DEBIAN_MIRROR@$BOOTSTRAP_DEBIAN_MIRROR@" \
+			"$MNTDIR/etc/apt/sources.list"
+		sed -i -e "s/FLAVOR/$BOOTSTRAP_FLAVOR/" "$MNTDIR/etc/apt/sources.list"
 
-	if [[ "$BOOTSTRAP_PARTITION_TYPE" == "msdos" ]]; then
-		desc_remove_setting "KVM_KERNEL"
-		desc_remove_setting "KVM_INITRD"
-		desc_remove_setting "KVM_APPEND"
-	else
-		desc_update_setting "KVM_APPEND" "root=$rootdev ro"
-	fi
+		if [ "$BOOTSTRAP_PARTITION_TYPE" = "msdos" ]; then
+			desc_remove_setting "KVM_KERNEL"
+			desc_remove_setting "KVM_INITRD"
+			desc_remove_setting "KVM_APPEND"
+		else
+			desc_update_setting "KVM_APPEND" "root=$rootdev ro"
+		fi
 
-	if [[ -n "$BOOTSTRAP_FINALIZE_COMMAND" ]]; then
-		eval "$BOOTSTRAP_FINALIZE_COMMAND"
-	fi	
+		if [ -n "$BOOTSTRAP_FINALIZE_COMMAND" ]; then
+			eval "$BOOTSTRAP_FINALIZE_COMMAND"
+		fi	
 
-	sync
-} 2>&1 | tee -a "$LOGFILE"
-}
+		sync
+	} 2>&1 | tee -a "$LOGFILE"
+} #bootstrap_fs()
 
